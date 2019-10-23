9f0cb538c4b70db3e4ef530bee94ae7c909b79ac
azinoVo <azinovo22@gmail.com>
2019-10-22 20:17:46 -0500

    added react and map

---
+++ src/coordinates.js
@@ -0,0 +1,23 @@
+const coordinates = [
+    {x: 60, y: 60},
+    {x: 60, y: 59},
+    {x: 60, y: 61},
+    {x: 61, y: 60},
+    {x: 59, y: 60},
+]
+
+const roadsCoords = [
+    {x: 60, y: 60},
+    {x: 60, y: 59},
+    {x: 60, y: 60},
+    {x: 60, y: 61},
+    {x: 60, y: 60},
+    {x: 59, y: 60},
+    {x: 61, y: 60},
+    {x: 60, y: 60},
+]
+
+export {
+    coordinates,
+    roadsCoords
+}
